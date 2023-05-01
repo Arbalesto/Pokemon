f669c60b73541aa77a709b4dea42e6841916b6fd
Arbalesto <kusiowskijan1@gmail.com>
2023-05-01 23:49:32 +0200

    feat: create Character Class and instance for this class

---
+++ FightGame/FightGame/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightGame
+{
+    internal class Character
+    {
+        public string name;
+        public double baseDmg;
+        public double defense;
+        public double healthPoints;
+        public int Stamina = 100;
+
+        public Character (string name, double baseDmg, double defense, double healthPoints)
+        {
+            this.name = name;
+            this.baseDmg = baseDmg;
+            this.defense = defense;
+            this.healthPoints = healthPoints;
+        }
+        public void showInfo()
+        {
+            Console.WriteLine("---INFORMATION---");
+            Console.WriteLine("NAME - {0}",name);
+            Console.WriteLine("DAMAGE - {0}",baseDmg);
+            Console.WriteLine("DEFENSE - {0}",defense);
+            Console.WriteLine("HP - {0}",healthPoints);
+            Console.WriteLine("Stamina - {0}", Stamina);
+        }
+    }
+   
+
+
+}
---
+++ FightGame/FightGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character Jasiu = new Character("Jasiu", 50, 50, 100);
+            Jasiu.showInfo();
+            Console.ReadKey();
+            
+        }
+    }
+}
